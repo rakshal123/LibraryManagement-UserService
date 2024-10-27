d4ca1864e2966d8c7aeedfbcdfc855c99d473793
sachinkumar-sde <sachinkumarb.sde@gmail.com>
2024-10-27 18:37:27 +0530

    Created User Service

--- UserService/src/main/java/com/libraryManager/UserService/controller/UserController.java
+++ UserService/src/main/java/com/libraryManager/UserService/controller/UserController.java
@@ -7,13 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api/v1")
 public class UserController {
 
     @Autowired
     private UserServiceImpl userService;
 
+    @PostMapping("/create-user")
     public ResponseEntity<UserDto> createUser(UserDto userDto, User user) {
         return new ResponseEntity<>(userService.createUser(userDto, user), HttpStatus.CREATED);
     }
